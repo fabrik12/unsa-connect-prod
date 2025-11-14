559bc53d215fe64a8582c4f73cd994d28ee1ef8d
fabrik12 <fbalarezo@unsa.edu.pe>
2025-11-14 14:59:12 -0500

    chore: remove unused index.js file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  register({ strapi }) {
-    console.log('🔥🔥🔥 STRAPI REGISTRADO - SRC/INDEX.JS 🔥🔥🔥');
-  },
-  bootstrap({ strapi }) {
-    console.log('🔥🔥🔥 STRAPI BOOTSTRAP - SRC/INDEX.JS 🔥🔥🔥');
-  },
-};
